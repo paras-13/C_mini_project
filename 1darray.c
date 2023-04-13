102d633761498a19d120063085532989acd6d29d
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-04-14 01:46:48 +0530

    1-D array algorithms

--- 1darray.c
+++ 1darray.c
@@ -121,7 +121,7 @@ void linear(int ls[], int n)
 }
 void binary(int ba[], int n)
 {
-    int num, flag=-1, end=n-1, beg=0, pos, mid ;
+    int num, end=n-1, beg=0, pos=-1, mid ;
     printf("Enter the number which you want to search using binary search");
     scanf("%d",&num);
     while (beg<=end)
@@ -136,19 +136,59 @@ void binary(int ba[], int n)
         {
             beg = mid+1;
         }
-        else if (ba[mid] < nu)
+        else if (ba[mid] < num)
         {
             end= mid-1;
         }
     }
+    if (pos != -1)
+    printf("Element %d found at %d location",num,pos+1);
+    else
+    printf("Element not found");
 }
 void delete(int arr[], int n)
 {
+    int ele, pos, flag = 0;
+    printf("Enter the element which you want to delete");
+    scanf("%d",&ele);
+     // Now first we will find out the position of that element
+    for (int i=0; i<n; i++)
+    {
+        if (arr[i] == ele)
+        {
+            pos = i; 
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+    {
+        for(int j=pos; j<n-1; j++)
+        {
+            arr[j] = arr[j+1];     
+        }
+        for (int k = 0; k<n; k++)
+        printf("%d  ",arr[k]);
+    }
+    else
+    printf("This element is not present in the given array");
 }
 void insert(int arr[], int n)
 {
+    int ele, pos;
+    printf("Enter the element which you want to insert in this array = ");
+    scanf("%d",&ele);
+    printf("Enter the position where you want to add this element");
+    scanf("%d",&pos);
+    for (int i=n-1; i>=pos-1; i--)
+    {
+        arr[i+1] = arr[i]; 
+    }
+    arr[pos-1] = ele;
+    printf("Displaying the new array\n");
+    for (int j=0; j<=n; j++)
+    printf("%d  ",arr[j]);
 }
 void merge(int arr[], int n)
 {
 }
-
