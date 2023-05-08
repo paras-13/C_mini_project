d94c04f2b0175fe7b3c638f7c7cd7b5664950c63
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-05-09 00:20:43 +0530

    2-D array in C

---
+++ 2darray.c
@@ -0,0 +1,8 @@
+/*
+ --> 2-Dimensional arrayA two-Dimanesional array is specified using two subscripts where one subscript denotes 
+     row and the other denottes column.
+ --> A one-dimensional array is organized linearly and only in one direction. 
+ --> But at times, we need to store data in the form of matrices or tables. Here the concept of 
+     one-dimension array is extended to incorporate two-dimansional data structures.
+
+*/
